ddfed8a8881b4402904a2cbc23351339071037e2
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2024-01-19 05:36:04 +0000

    Cleaned dataset, removed modeling file

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,12 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(readxl)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+g <- "guess"
+dataset <- "inputs/data/ttc-bus-delay-data-2023.xlsx"
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+types <- c(g, "text", g, g, g, g, g, g, g, g)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+raw_data <- read_excel(dataset, col_types = types)
