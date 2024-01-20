df8158b638f1cc6b12017c1ea435ea66fa3dee01
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2024-01-20 03:12:17 +0000

    More data cleaning - Combining date/time data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -10,11 +10,25 @@
 #### Workspace setup ####
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
 #### Clean data ####
+
+# Variable creation
 g <- "guess"
 dataset <- "inputs/data/ttc-bus-delay-data-2023.xlsx"
-
 types <- c(g, "text", g, g, g, g, g, g, g, g)
 
+# Reading dataset
 raw_data <- read_excel(dataset, col_types = types)
+
+# Creating new DateTime column, removing old Date and Time columns
+raw_data <- raw_data %>%
+  mutate(DateTime = make_datetime(year(Date), month(Date), day(Date),
+                                  hour(hm(Time)), minute(hm(Time))))
+
+raw_data <- raw_data %>% relocate(DateTime)
+raw_data$Time <- NULL
+raw_data$Date <- NULL
+
+raw_data
